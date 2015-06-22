6c2e24d6c4fd6286a8a9d1aeeb586c50aa77a82f
nikitasmall <nikitasosnov@yahoo.com>
2015-06-22 12:04:09 +0300

    adding of new tasks implemented

--- api/controllers/TaskController.js
+++ api/controllers/TaskController.js
@@ -8,6 +8,23 @@
 module.exports = {
 	// list of tasks action
 	index: function (req, res) {
-		return res.view('task/index')
+		// find all the records
+		Task.find({}, function(err, tasks) {
+			// and show them into the view
+			return res.view('task/index',{
+				tasks: tasks
+			});
+		});
+	},
+
+	// create a new task
+	createTask: function (req, res) {
+		Task.create({
+			title: req.param('title', 'Unnamed'),
+			description: req.param('description', ''),
+			complete: false
+		}, function(err) {
+			return res.redirect('/');
+		});
 	}
 };
